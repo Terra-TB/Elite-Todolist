7aa8b89bb0dfb1af109832b6cfd84ced5bfd4c6a
Terra-TB <allen.cachat@gmail.com>
2026-02-25 10:12:58 -0500

    Update Bar.js

--- Classes/Bar.js
+++ Classes/Bar.js
@@ -5,16 +5,11 @@ class Bar{
         this.width = init_width;
         this.height = init_height;
         this.cornerCouverture = init_cornerCouverture;
-        this.color = init_color;
-        this.stroke = 0;
+        this.color = init_color
 
         this.addListButton = createButton(`Add list`);
         this.addListButton.hide();
         this.addListButton.mousePressed(() => this.buttonPressedMakeList());
-
-        this.addDarkButton = createButton(`Dark Mode`);
-        this.addDarkButton.hide();
-        this.addDarkButton.mousePressed(() => this.buttonPressedToggleDarkMode());
     }
 
     buttonPressedMakeList(){
@@ -33,37 +28,24 @@ class Bar{
         saveAllLists();
     }
 
-    buttonPressedToggleDarkMode(){
-        // console.log("EVIL MODE ACTIVATED");
-        //this.color = "#000000";
-        //this.stroke = "#FFFFFF";
-    }
 
     show(){
-        stroke(this.stroke);
+        stroke(0);
         fill(this.color);
         rect(this.x, this.y, this.width, this.height, this.cornerCouverture);
 
         
         //sets pos of buttons
-        let xOffest = 40;
-        let yOffest = this.height/4;
+        let xOffest = 40
+        let yOffest = this.height/4
         this.addListButton.position(this.x + xOffest, this.y + yOffest);
-
-        this.addDarkButton.position(this.x + this.width - xOffest * 3.5, this.y + yOffest);
 
         this.addListButton.style("padding", "12px 20px"); 
         this.addListButton.style("font-size", "20px"); 
 
-        this.addDarkButton.style("padding", "12px 20px"); 
-        this.addDarkButton.style("font-size", "20px"); 
-
-        styleButton(this.addListButton);
-
-        styleButton(this.addDarkButton);
+        styleButton(this.addListButton)
         //shows buttons
         this.addListButton.show();
-        this.addDarkButton.show();
     }
     
 }
